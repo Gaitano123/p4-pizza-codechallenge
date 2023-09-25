51b3a8e1ccbd6de05392f26cac3a2801744e7a79
Gaitano <gaitano.george@student.moringaschool.com>
2023-09-25 18:33:02 +0300

    Documented using swagger

--- pizza/api/__init__.py
+++ pizza/api/__init__.py
@@ -6,6 +6,7 @@ from flask_restful import Resource, Api
 from sqlalchemy.orm import validates
 from sqlalchemy import MetaData
 from flask_sqlalchemy import SQLAlchemy
+from flask_swagger_ui import get_swaggerui_blueprint
 
 app = Flask(__name__)
 api = Api(app)
@@ -19,3 +20,25 @@ migrate = Migrate(app, db)
 db.init_app(app)
 
 
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = '/static/openapi.json'  # Our API url (can of course be a local resource)
+
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+app.register_blueprint(swaggerui_blueprint)
